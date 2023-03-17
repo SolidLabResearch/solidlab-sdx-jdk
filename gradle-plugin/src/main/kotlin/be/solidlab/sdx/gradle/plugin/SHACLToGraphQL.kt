89cae412aebcecded2e9106cfb0181534176312b
Wannes Kerckhove <wannes.kerckhove@ugent.be>
2023-03-17 16:33:38 +0100

    feat: started working on mutations

--- gradle-plugin/src/main/kotlin/be/solidlab/sdx/gradle/plugin/SHACLToGraphQL.kt
+++ gradle-plugin/src/main/kotlin/be/solidlab/sdx/gradle/plugin/SHACLToGraphQL.kt
@@ -1,12 +1,15 @@
 package be.solidlab.sdx.gradle.plugin
 
+import com.apollographql.apollo3.compiler.capitalizeFirstLetter
 import graphql.Scalars
 import graphql.introspection.Introspection
 import graphql.schema.*
 import graphql.schema.idl.SchemaPrinter
 import org.apache.jena.graph.Graph
 import org.apache.jena.graph.Node
+import org.apache.jena.graph.NodeFactory
 import org.apache.jena.graph.Node_URI
+import org.apache.jena.graph.Triple
 import org.apache.jena.riot.RDFDataMgr
 import org.apache.jena.shacl.Shapes
 import org.apache.jena.shacl.engine.TargetType
@@ -16,11 +19,14 @@ import org.apache.jena.shacl.parser.NodeShape
 import org.apache.jena.shacl.parser.PropertyShape
 import org.apache.jena.shacl.vocabulary.SHACL
 import org.apache.jena.sparql.graph.GraphFactory
+import org.apache.jena.vocabulary.RDF
 import org.apache.jena.vocabulary.XSD
 import org.gradle.api.file.Directory
 import java.net.URI
 import java.util.*
 import kotlin.jvm.optionals.getOrNull
+
+val importedFrom = NodeFactory.createURI("https://solidlab.be/vocab/importedFrom")
 
 val identifierDirective: GraphQLDirective = GraphQLDirective.newDirective()
     .name("identifier")
@@ -33,12 +39,12 @@ val idField: GraphQLFieldDefinition = GraphQLFieldDefinition.newFieldDefinition(
     .type(GraphQLNonNull.nonNull(Scalars.GraphQLID))
     .withAppliedDirective(identifierDirective.toAppliedDirective())
     .build()
+
 val isDirective: GraphQLDirective = GraphQLDirective.newDirective()
     .name("is")
     .argument(GraphQLArgument.newArgument().name("class").type(Scalars.GraphQLString))
     .validLocation(Introspection.DirectiveLocation.OBJECT)
     .build()
-
 val propertyDirective: GraphQLDirective = GraphQLDirective.newDirective()
     .name("property")
     .argument(GraphQLArgument.newArgument().name("iri").type(Scalars.GraphQLString))
@@ -46,21 +52,41 @@ val propertyDirective: GraphQLDirective = GraphQLDirective.newDirective()
     .build()
 
 object SHACLToGraphQL {
-    fun getSchema(shaclDir: Directory): String {
+    fun getSchema(shaclDir: Directory, shapeImports: List<ShapeImport>): String {
         println("Looking at Shape definitions in ${shaclDir.asFile.path}")
         val graph = GraphFactory.createDefaultGraph()
-        shaclDir.asFile.listFiles()?.forEach {
-            println("  --> Importing ${it.path} into graph")
-            RDFDataMgr.read(graph, it.toURI().toString())
+        shapeImports.forEach { shapeImport ->
+            println("  --> Importing ${shapeImport.importUrl} into graph")
+            val shapeSubGraph = GraphFactory.createDefaultGraph()
+            RDFDataMgr.read(shapeSubGraph, shaclDir.file(shapeImport.getTargetFileName()).asFile.toURI().toString())
+            shapeSubGraph.find().filterDrop { it.subject.isURI && shapeImport.exclude.contains(it.subject.uri) }
+                .forEach {
+                    if (it.predicateMatches(RDF.type.asNode())) {
+                        graph.add(Triple.create(it.subject, importedFrom, NodeFactory.createURI(shapeImport.importUrl)))
+                    }
+                    graph.add(it)
+                }
         }
-        val context = ParseContext(Shapes.parse(graph))
+        val context =
+            ParseContext(Shapes.parse(graph), shapeImports.associateBy { NodeFactory.createURI(it.importUrl) })
 
         println("Building GraphQL schema from Graph")
+        println("  --> Constructing GraphQL object types")
         val graphQLTypes = context.allShapes.iteratorAll().asSequence()
             .map { shape -> generateObjectType(shape as NodeShape, context) }.distinctBy { it.name }.toSet()
 
+        println("  --> Constructing GraphQL input types")
+        val graphQLInputTypes = context.allShapes.iteratorAll().asSequence()
+            .filter { shape -> context.getImportDefinition(shape.shapeNode).generateMutations }
+            .flatMap { shape ->
+                InputTypeConfiguration.values().map { generateInputType(shape as NodeShape, context, it) }
+            }
+            .distinctBy { it.name }.toSet()
+
+        println("  --> Assembling the GraphQL schema")
         val schema = GraphQLSchema.newSchema()
             .query(generateEntryPoints(graphQLTypes))
+            .mutation(generateMutations(graphQLInputTypes))
             .additionalTypes(graphQLTypes)
             .additionalDirectives(setOf(isDirective, propertyDirective, identifierDirective))
             .build()
@@ -88,8 +114,12 @@ fun generateEntryPoints(types: Set<GraphQLObjectType>): GraphQLObjectType {
         .build()
 }
 
+fun generateMutations(types: Set<GraphQLInputObjectType>): GraphQLObjectType {
+    TODO()
+}
+
 fun generateObjectType(shape: NodeShape, context: ParseContext): GraphQLObjectType {
-    val shapeName = parseName(shape.shapeNode.uri)
+    val shapeName = parseName(shape.shapeNode, context)
     val childContext = context.copy(nodeShape = shape)
 
     return GraphQLObjectType.newObject()
@@ -103,6 +133,66 @@ fun generateObjectType(shape: NodeShape, context: ParseContext): GraphQLObjectTy
             }
         )
         .fields(listOf(idField).plus(shape.propertyShapes.mapNotNull { generateFieldDefinition(it, childContext) }))
+        .build()
+}
+
+fun generateInputType(
+    shape: NodeShape,
+    context: ParseContext,
+    inputTypeConfiguration: InputTypeConfiguration
+): GraphQLInputObjectType {
+    val shapeName = parseName(shape.shapeNode, context)
+    return GraphQLInputObjectType.newInputObject()
+        .name("$inputTypeConfiguration${shapeName}Input")
+        .withAppliedDirective(
+            isDirective.toAppliedDirective().transform {
+                it.argument(
+                    GraphQLAppliedDirectiveArgument.newArgument().name("class").type(Scalars.GraphQLString)
+                        .valueProgrammatic(shape.targets.find { it.targetType == TargetType.targetClass }?.`object`?.uri)
+                )
+            }
+        )
+        .fields(shape.propertyShapes
+            // Only include literal properties
+            .filter { property -> property.shapeGraph.getReference(property.shapeNode, SHACL.datatype) != null }
+            .map { property ->
+                val propName =
+                    property.shapeGraph.getString(property.shapeNode, SHACL.name) ?: propertyNameFromPath(
+                        property.path.toString().removeSurrounding("<", ">")
+                    )
+                val description = property.shapeGraph.getString(property.shapeNode, SHACL.description)
+                val collection =
+                    property.constraints.any { it is MaxCount && it.maxCount > 1 } || property.constraints.none { it is MaxCount }
+                val minCount = property.constraints.find { it is MinCount }?.let { (it as MinCount).minCount } ?: 0
+                val dataType = generateScalar(property.shapeGraph.getReference(property.shapeNode, SHACL.datatype)!!)
+                val effectiveType = when (inputTypeConfiguration) {
+                    InputTypeConfiguration.CREATE_TYPE -> if (collection) {
+                        GraphQLNonNull.nonNull(GraphQLList.list(GraphQLNonNull.nonNull(dataType)))
+                    } else {
+                        if (minCount > 0) GraphQLNonNull(dataType) else dataType
+                    }
+
+                    InputTypeConfiguration.UPDATE_TYPE -> if (collection) {
+                        GraphQLList.list(GraphQLNonNull.nonNull(dataType))
+                    } else {
+                        dataType
+                    }
+                }
+                GraphQLInputObjectField.newInputObjectField()
+                    .name(propName)
+                    .description(description)
+                    .type(effectiveType)
+                    .withAppliedDirective(
+                        propertyDirective.toAppliedDirective().transform {
+                            it.argument(
+                                GraphQLAppliedDirectiveArgument.newArgument().name("iri").type(Scalars.GraphQLString)
+                                    .valueProgrammatic(property.path)
+                            )
+                        }
+                    )
+                    .build()
+            }
+        )
         .build()
 }
 
@@ -122,7 +212,7 @@ fun generateFieldDefinition(property: PropertyShape, context: ParseContext): Gra
     } else if (classRef != null) {
         val matchingShape = context.allShapes.getMatchingShape(classRef)
         if (matchingShape != null) {
-            GraphQLTypeReference(parseName(matchingShape.shapeNode.uri))
+            GraphQLTypeReference(parseName(matchingShape.shapeNode, context))
         } else {
             println("  --> Property '$propName' for Shape <${context.nodeShape?.shapeNode?.uri}> has no matching Shape definition (for sh:class <${classRef.uri}>) and will be ignored!")
             null
@@ -134,7 +224,7 @@ fun generateFieldDefinition(property: PropertyShape, context: ParseContext): Gra
 
     return if (rawGraphQLType != null) {
         val graphQLType = if (collection) {
-            GraphQLNonNull.nonNull(GraphQLList.list(rawGraphQLType))
+            GraphQLNonNull.nonNull(GraphQLList.list(GraphQLNonNull.nonNull(rawGraphQLType)))
         } else {
             if (minCount > 0) GraphQLNonNull(rawGraphQLType) else rawGraphQLType
         }
@@ -165,10 +255,12 @@ fun generateScalar(dataType: Node_URI): GraphQLScalarType {
     }
 }
 
-fun parseName(uriStr: String): String {
-    val uri = URI(uriStr)
+fun parseName(subject: Node, context: ParseContext): String {
+    val shapeImport = context.getImportDefinition(subject)
+    val uri = URI(subject.uri)
     // If the URI has a fragment, use fragment, otherwise use the last path segment
-    return uri.fragment ?: uri.path.substringAfterLast("/")
+    val name = uri.fragment ?: uri.path.substringAfterLast("/")
+    return "${shapeImport.typePrefix.capitalizeFirstLetter()}${name.capitalizeFirstLetter()}"
 }
 
 @OptIn(ExperimentalStdlibApi::class)
@@ -199,7 +291,23 @@ fun propertyNameFromPath(path: String): String {
     }
 }
 
-data class ParseContext(val allShapes: Shapes, val nodeShape: NodeShape? = null)
+data class ParseContext(
+    val allShapes: Shapes,
+    val shapeImports: Map<Node, ShapeImport>,
+    val nodeShape: NodeShape? = null
+) {
+
+    fun getImportDefinition(subject: Node): ShapeImport {
+        val importNode = allShapes.graph.find(subject, importedFrom, Node.ANY).toList()
+            .firstOrNull()?.`object`
+        return shapeImports[importNode]!!
+    }
+
+}
+
+enum class InputTypeConfiguration(val typePrefix: String) {
+    CREATE_TYPE("Create"), UPDATE_TYPE("Update")
+}
 
 private fun decapitalize(str: String): String {
     return str.replaceFirstChar { it.lowercase(Locale.getDefault()) }
--- gradle-plugin/src/main/kotlin/be/solidlab/sdx/gradle/plugin/SolidGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/be/solidlab/sdx/gradle/plugin/SolidGradlePlugin.kt
@@ -13,21 +13,33 @@ import com.apollographql.apollo3.gradle.internal.DefaultApolloExtension
 const val DEFAULT_CATALOG = "https://catalog.solidlab.be/"
 
 interface SdxExtension {
-    val source: Property<String>
-    val additionalSources: ListProperty<String>
+    // Shorthand for importShapes: imports the Shapes from the specified URLs using the default options.
     val importShapesFromURL: ListProperty<String>
 
+    // List of the Shape imports
+    val importShapes: ListProperty<ShapeImport>
+
+    // Name of the package to use for the generated classes.
     val packageName: Property<String>
 //    val importShapes: ListProperty<ShapeImportResource>
 }
-//
-//data class ShapeImportResource(
-//    val packageId: String,
-//    val versionId: String
-//)
 
-interface ApolloExtension {
-    val packageName: Property<String>
+data class ShapeImport(
+    // URL of the Shape package or file to import
+    val importUrl: String,
+    // Set of Shapes to ignore, identified by their URIs.
+    val exclude: Set<String> = emptySet(),
+    // Determines if mutations should be generated for the Shapes
+    val generateMutations: Boolean = true,
+    // An optional prefix to add to types generated from the to be imported Shapes, can help to solve conflicts.
+    val typePrefix: String = "",
+    // An optional filename to use for the downloaded Shape import, can help to solve conflicts.
+    val shapeFileName: String? = null
+) {
+    fun getTargetFileName(): String {
+        val importUrl = URL(importUrl)
+        return this.shapeFileName ?: (importUrl.path.substringAfterLast("/"))
+    }
 }
 
 class SolidGradlePlugin : Plugin<Project> {
@@ -36,24 +48,27 @@ class SolidGradlePlugin : Plugin<Project> {
 
         project.task("sdxBuild") {
             it.doFirst {
-                val additionalSources =
-                    extension.additionalSources.orNull?.takeIf { it.isNotEmpty() }
-                        ?.let { " + ${it.joinToString()} (additional)" } ?: ""
-                println("Installing shapes using sources: ${extension.source.getOrElse(DEFAULT_CATALOG)} (main)$additionalSources")
-                val shapeDir = project.layout.projectDirectory.dir("src/main/shapes")
-                shapeDir.asFile.mkdirs()
-                extension.importShapesFromURL.get().map { URL(it) }.forEach { shapeImportURL ->
+                println("Installing shapes...")
+                val shapeDir = project.layout.buildDirectory.dir("sdx/shapes")
+                shapeDir.get().asFile.mkdirs()
+                val shapeImports = extension.importShapesFromURL.get().map { url -> ShapeImport(importUrl = url) }
+                    .plus(extension.importShapes.get())
+
+                // Download the Shapes
+                shapeImports.forEach { shapeImport ->
+                    val shapeImportURL = URL(shapeImport.importUrl)
                     println("  Starting download for $shapeImportURL")
                     downloadFile(
-                        shapeImportURL,
-                        shapeDir.file(shapeImportURL.path.substringAfterLast("/")).asFile.absolutePath
+                        URL(shapeImport.importUrl),
+                        shapeDir.get().file(shapeImport.getTargetFileName()).asFile.absolutePath
                     )
                     println("  --> Done!")
                 }
-                println("Generating GraphQL schema from installed Shapes")
+
+                println("Generating GraphQL schema from installed Shapes...")
                 val graphqlDir = project.layout.projectDirectory.dir("src/main/graphql")
                 graphqlDir.asFile.mkdirs()
-                val schema = SHACLToGraphQL.getSchema(shapeDir)
+                val schema = SHACLToGraphQL.getSchema(shapeDir.get(), shapeImports)
                 Files.writeString(graphqlDir.file("schema.graphqls").asFile.toPath(), schema)
             }
         }
